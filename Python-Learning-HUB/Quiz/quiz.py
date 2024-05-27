0295e15ff326ad6094b7f750798a8c443b2cdcf1
Pedro Augusto <apedro.rp@gmail.com>
2024-05-27 14:38:39 -0300

    new file:   JavaScript-Learning-HUB 	new file:   Python-Learning-HUB/Quiz/quiz.py 	new file:   Python-Learning-HUB/Quiz/quiz_v2.py

---
+++ Python-Learning-HUB/Quiz/quiz.py
@@ -0,0 +1,29 @@
+print('Mensagem de boas vindas')
+answerUser = input ('Deseja começar? (S/N)')
+
+if answerUser.upper() != 'S':
+    quit()
+
+score = 0
+
+print ('Comecando...')
+
+print ('Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Actvision \n (D) EA \n')
+answer_1 = input('Resposta: ')
+
+if answer_1.upper() == 'A': 
+    print ('Parabéns!!! Você acertou!')
+    score = score + 1
+else:
+    print (f'Que pena! Você errou... A resposta correta é a letra A')
+
+print ('Qual o nome do protagonista em GTA SAn Andreas?\n (A) Carlos John \n (B) Carl Johnson \n (C) Cassius Joe  \n (D) Carlos Jay \n')
+answer_2 = input('Resposta: ')
+if answer_2.upper() == 'B': 
+    print ('Parabéns!!! Você acertou!')
+    score = score + 1
+else:
+    print (f'Que pena! Você errou... A resposta correta é a letra B')
+
+print(f'O Quiz acabou... Pontuação: {score}/2')
+       
---
+++ Python-Learning-HUB/Quiz/quiz_v2.py
@@ -0,0 +1,39 @@
+print('Bem-vindo ao Quiz!')
+
+answer_user = input('Deseja começar? (S/N): ').upper()
+
+if answer_user != 'S':
+    quit()
+
+score = 0
+total_questions = 2
+
+print('Começando...')
+
+# Dicionário de perguntas e respostas
+questions = {
+    "Quem desenvolveu o jogo Grand Theft Auto (GTA)?": {
+        "opcoes": ["A) Rockstar Games", "B) Ubisoft", "C) Activision", "D) EA"],
+        "resposta": "A"
+    },
+    "Qual o nome do protagonista em GTA San Andreas?": {
+        "opcoes": ["A) Carlos John", "B) Carl Johnson", "C) Cassius Joe", "D) Carlos Jay"],
+        "resposta": "B"
+    }
+}
+
+# Loop para iterar sobre as perguntas
+for index, (pergunta, detalhes) in enumerate(questions.items(), start=1):
+    print(f'\nPergunta {index}/{total_questions}:')
+    print(pergunta)
+    for opcao in detalhes["opcoes"]:
+        print(opcao)
+    resposta = input('Resposta: ').upper()
+
+    if resposta == detalhes["resposta"]:
+        print('Parabéns!!! Você acertou!')
+        score += 1
+    else:
+        print(f'Que pena! Você errou... A resposta correta é a letra {detalhes["resposta"]}')
+
+print(f'\nO Quiz acabou... Pontuação: {score}/{total_questions}')
