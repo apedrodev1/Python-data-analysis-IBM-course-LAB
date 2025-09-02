7326599ecd52d889fa35f1ed03705193f312b07c
Pedro Augusto <apedro.rp@gmail.com>
2025-09-02 13:09:24 -0300

    feat: writing mode

---
+++ Python-data-analysis-IBM-course-LAB/Modos/Writing 'w'/writing_file.py
@@ -0,0 +1,5 @@
+# Writing mode 'w' - create or overscribed data
+with open ('../notes.txt', 'w') as File1:
+    File1.write("Pedro - 8.5\n")
+    File1.write("Luis - 5.2\n")
+    File1.write("Alex - 9.0\n")
