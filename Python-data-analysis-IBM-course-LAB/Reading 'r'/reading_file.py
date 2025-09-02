41797e1a087f0809aa5646c393b1593552067773
Pedro Augusto <apedro.rp@gmail.com>
2025-09-02 13:07:47 -0300

    deleted files

--- Python-data-analysis-IBM-course-LAB/Reading 'r'/reading_file.py
+++
@@ -1,5 +0,0 @@
-# Reading mode
-with open("notes.txt", "r") as file1:
-    file_stuff = file1.readlines()
-    # file_stuff = file1.readline(7)
-    print(file_stuff)
--- Python-data-analysis-IBM-course-LAB/Writing 'w'/writing_file.py
+++
@@ -1,4 +0,0 @@
-# Writing mode 
-with open ('notes.txt', 'w') as File1:
-    File1.write ('This is line b') 
-    File1.write('This is line z')
